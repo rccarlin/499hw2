4a4668ae3b3a4e820fdf60c7ee829d9ed5e39e05
Riley Carlin <rcarlin@usc.edu>
2022-10-14 14:21:40 -0500

    write up

--- model.py
+++ model.py
@@ -4,46 +4,17 @@ import torch.nn as nn
 
 
 class skipGram(nn.Module):
-    def __init__(self, vocabSize, embedDim):
+    def __init__(self, numWords, embedDim):
         super(skipGram,self).__init__()
-        self.numWords = vocabSize
-        # self.hidDim fixme should I choose how many hidden dimensions?
-        self.embeddingDim = embedDim  # fixme that is something u can choose
-        self.embed = nn.Embedding(self.numWords, self.embeddingDim)
-        self.bound = .5 / self.embeddingDim
-        self.toOut = nn.Linear(self.embeddingDim, self.numWords)
-        # self.toHidden = (-2 * self.bound) * torch.rand(self.numWords, self.embeddingDim,
-        #                                                requires_grad=True) + self.bound
-        #
-        # # FIXME do I need to make that float? isn't it already?
-        # self.toOut = (-2 * self.bound) * torch.rand(self.embeddingDim, self.numWords,
-        #                                             requires_grad=True) + self.bound
-
-    def inputOneHot(self, curr):
-        # I want a matrix that has a zero for every word my current word isn't, and 1 for curr word
-        oneHot = torch.zeros(self.numWords).long()
-        oneHot[curr] = 1.0
-        return oneHot
-        # FIXME how do I ensure curr is the index of a word?do I even need this?
+        self.vocab_size = numWords
+        self.embedding_dim = embedDim
+        self.embed = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.toOut = nn.Linear(self.embedding_dim, self.vocab_size)
 
     def forward(self, curr):
-        # know curr is a one hot
         curr = self.embed(curr)
         out = self.toOut(curr)
         # hid = curr.mm(self.toHidden)
         # out = hid.mm(self.toOut)
         return out
 
-#
-#
-#     def forward(self, command):
-#         embeds = self.embeddings(command)  # command should already be a list of embedded words
-#         lstmOut, _ = self.lstm(embeds.view(len(command[0]), 1, -1))
-#
-#         # calculating scores for different possible labels
-#         actSpace = self.toAct(lstmOut.view(len(command[0]), -1))
-#         targetSpace = self.toTarget(lstmOut.view(len(command[0]), -1))
-#         actScores = F.log_softmax(actSpace, dim=1)
-#         targetScores = F.log_softmax(targetSpace, dim=1)
-
-       #return actScores[len(actScores) - 1], targetScores[len(targetScores) - 1]
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
 import gensim
 import tqdm
+import torch
 
 
 def read_analogies(analogies_fn):
@@ -24,3 +25,19 @@ def save_word2vec_format(fname, model, i2v):
                     "%s %s\n" % (word, " ".join("%f" % val for val in row))
                 )
             )
+
+def get_device(force_cpu, status=True):
+    # if not force_cpu and torch.backends.mps.is_available():
+    # 	device = torch.device('mps')
+    # 	if status:
+    # 		print("Using MPS")
+    # elif not force_cpu and torch.cuda.is_available():
+    if not force_cpu and torch.cuda.is_available():
+        device = torch.device("cuda")
+        if status:
+            print("Using CUDA")
+    else:
+        device = torch.device("cpu")
+        if status:
+            print("Using CPU")
+    return device
